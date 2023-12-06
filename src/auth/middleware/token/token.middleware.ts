b7847778fd4c2f3493dc066ab89b0601ef85e72a
yzk-home <yasin@compassfairs.no>
2023-12-06 18:37:09 +0100

    implement basic token validation

---
+++ src/auth/middleware/token/token.middleware.ts
@@ -0,0 +1,34 @@
+// token.middleware.ts
+import { Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+enum AccessPoint {
+  Token = '123',
+};
+
+@Injectable()
+export class TokenMiddleware implements NestMiddleware {
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      throw new UnauthorizedException('Invalid or missing token');
+    }
+
+    const token = authHeader.substring(7); 
+    
+    try {
+
+      let validate : string = AccessPoint.Token;
+
+      if (token === validate) {
+        next();
+      } else {
+        throw new UnauthorizedException('Invalid token');
+      }
+    } catch (err) {
+      throw new UnauthorizedException('Invalid token');
+    }
+  }
+}
